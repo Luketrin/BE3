cb67abc5a821880209a8e1ffcc4059ec3d4aa6b6
Luketrin <luketrinity23@gmail.com>
2024-08-01 16:37:35 -0300

    Index, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+const lunchButton = document.getElementById('please');
+
+lunchButton.addEventListener('click', () => {
+  window.open('https://youtu.be/cF1Na4AIecM?si=TkU5z6eChQy7J8pE');
+});
+
+const chihiroButton = document.getElementById('espresso');
+
+chihiroButton.addEventListener('click', () => {
+  window.open('https://youtu.be/eVli-tstM5E?si=B3nEOLE8rUpmy5sv');
+});
